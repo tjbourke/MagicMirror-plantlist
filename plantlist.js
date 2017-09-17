7c89d9b6a104d2a6bc3024149a034fb2954b2c4a
TJ Bourke <tbourke@flexngate-mi.com>
2017-09-17 11:43:15 -0400

    Try pulling plant name from API

--- plantlist.js
+++ plantlist.js
@@ -1,13 +1,73 @@
 Module.register("plantlist",{
 	// Default module config.
 	defaults: {
-		text: "Hello World!"
+		text: "Hello World!",
+
+		animationSpeed: 1000,
+
+		apiBase: 'http://192.168.1.113/api/',
+		apiVersion: 'v1',
+		endPoint: 'plants'
+	},
+
+	start: function()
+	{
+		/*if (this.config.appid === "") {
+			Log.error("CurrentWeather: APPID not set!");
+			return;
+		}*/
+
+		var url = this.config.apiBase + this.config.apiVersion + "/" + this.config.endpoint;// + this.getParams();
+		//var url = this.config.apiBase + this.config.apiVersion + "/" + this.config.weatherEndpoint + this.getParams();
+		var self = this;
+		var retry = true;
+
+		var request = new XMLHttpRequest();
+		request.open("GET", url, true);
+		request.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status === 200) {
+					self.processList(JSON.parse(this.response));
+				} else if (this.status === 401) {
+					//self.updateDom(self.config.animationSpeed);
+
+					//Log.error(self.name + ": Incorrect APPID.");
+					retry = true;
+				} else {
+					//Log.error(self.name + ": Could not load weather.");
+				}
+
+				if (retry) {
+					//self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
+				}
+			}
+		};
+		request.send();
+	},
+
+	processList: function(data)
+	{
+		if (!data) { // || !data.main || typeof data.main.temp === "undefined") {
+			// Did not receive usable new data.
+			// Maybe this needs a better check?
+			return;
+		}
+
+		plants = data.data;
+
+		this.show(this.config.animationSpeed, {lockString:this.identifier});
+		//this.loaded = true;
+
+		this.plantName = plants[0]['facility_name'];
+
+		this.updateDom(this.config.animationSpeed);
+		//this.sendNotification("CURRENTWEATHER_DATA", {data: data});
 	},
 
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
+		wrapper.innerHTML = this.config.plantName;
 		return wrapper;
 	}
 });
